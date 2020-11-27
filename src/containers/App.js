c3b82d7927bb85239777d0a4affa616ced3dc11f
onanalee <69330162+onanalee@users.noreply.github.com>
2020-11-27 10:21:14 +0900

    Folder arrangement
    1. Folder rearrangement (containers, components)
    2. Build folder created.
    3. Code clean up

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox'
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll';
 import './App.css'
-import Scroll from './Scroll'
-import { robots } from './Robots';
+
 
 class App extends Component {
     constructor () {
@@ -26,10 +26,12 @@ class App extends Component {
         this.setState({ searchfield : event.target.value })
     }
     render () {
-        const filteredRobots = this.state.robots.filter(robot => {
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        //destructuring
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0){
+        if (!robots.length){
             return <h1>Loading...</h1>
         } else {
             return (
